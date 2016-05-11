9ad9a5d7e7848dfee2207a5d4b82002f5eb874da
arie256 <arie256@users.noreply.github.com>
2016-05-11 17:01:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ module.exports = new Script({
                 }
 
                 if (!_.has(scriptRules, upperText)) {
-                    return bot.say(`Grrrr.  Sorry, I do not understand "` + upperText + `".`).then(() => 'speak');
+                    return bot.say('Grrrr.  Sorry, I do not understand "' + upperText + '".').then(() => 'speak');
                 }
 
                 var response = scriptRules[upperText];
